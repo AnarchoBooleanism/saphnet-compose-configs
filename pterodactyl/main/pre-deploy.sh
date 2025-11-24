4f4b367db25c172cb36deec4991065c19401a355
Nathan Guerrero <naguerr2@uci.edu>
2025-11-24 04:39:27 +0000

    fix: Fix syntax for pre-deploy script of pterodactyl

--- pterodactyl/main/pre-deploy.sh
+++ pterodactyl/main/pre-deploy.sh
@@ -7,7 +7,7 @@ export NFS_PATH="/mnt/saphnet-nas1a/pterodactyl-data"
 
 mkdir -p $LOCAL_MOUNT_PATH
 
-if [[ ! grep -qs "$LOCAL_MOUNT_PATH" filename ]] ; then
+if ! grep -qs "$LOCAL_MOUNT_PATH" filename ; then
   mount -t nfs -o addr=$NFS_HOST,nolock,soft,rw,nfsvers=4 \
     $NFS_HOST:$NFS_PATH $LOCAL_MOUNT_PATH
 fi
