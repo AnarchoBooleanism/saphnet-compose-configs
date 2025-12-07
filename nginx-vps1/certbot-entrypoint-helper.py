de07aab1aa25ba3c0776e863d0ee77ff1d87f3aa
Nathan Guerrero <naguerr2@uci.edu>
2025-12-07 19:57:34 +0000

    fix: Fix how namecheap.ini is written in nginx-vps1 certbot entrypoint script, also add more print statements

--- nginx-vps1/certbot-entrypoint-helper.py
+++ nginx-vps1/certbot-entrypoint-helper.py
@@ -3,18 +3,22 @@ import os, subprocess
 
 # Create /namecheap.ini
 with open("/namecheap.ini", "a") as file:
-    file.write(f"dns_namecheap_username={os.environ["DNS_NAMECHEAP_USERNAME"]}")
-    file.write(f"dns_namecheap_api_key={os.environ["DNS_NAMECHEAP_API_KEY"]}")
+    print(f"Namecheap username is {os.environ["DNS_NAMECHEAP_USERNAME"]}")
+    file.write(f"dns_namecheap_username={os.environ["DNS_NAMECHEAP_USERNAME"]}\n")
+    print(f"Namecheap API key is {os.environ["DNS_NAMECHEAP_API_KEY"]}")
+    file.write(f"dns_namecheap_api_key={os.environ["DNS_NAMECHEAP_API_KEY"]}\n")
 
 # Check that all certificates for all domains exist, if not then create them
 domain_array = os.environ["CERTBOT_DOMAINS"].split(" ")
 missing_domains = []
 
+print("Checking domains " + ",".join(domain_array))
 for domain in domain_array:
     if not os.path.exists(f"/etc/lets_encrypt/live/{domain}/fullchain.pem"):
         missing_domains.append(domain)
 
 if missing_domains:
+    print("Running Certbot to create certificates for " + ",".join(missing_domains) + "...")
     subprocess.call([
         "certbot", "certonly",
         "-a", "dns-namecheap",
@@ -23,3 +27,5 @@ if missing_domains:
         "--no-eff-email", "--email", os.environ["CERTBOT_EMAIL"],
         "--domains", ",".join(missing_domains)
     ])
+else:
+    print("No domains without certificates exist. Continuing...")
