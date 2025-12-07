b04dcc6eaca5a83f0e4fdfa72fe69cbb0d59a34f
Nathan Guerrero <naguerr2@uci.edu>
2025-12-07 18:57:22 +0000

    feat: Special NGINX configuration for vps1
    For pterodactyl wing, clear up a comment and use bash instead of sh for entrypoint

--- nginx-vps1/certbot-entrypoint.sh
+++ nginx-vps1/certbot-entrypoint.sh
@@ -3,21 +3,33 @@ set -eu
 
 trap "exit" TERM
 
+domain_array=($CERTBOT_DOMAINS)
+
 # Create /namecheap.ini
 cat > /namecheap.ini <<EOF
 dns_namecheap_username=$DNS_NAMECHEAP_USERNAME
 dns_namecheap_api_key=$DNS_NAMECHEAP_API_KEY
 EOF
 
-# First-time certificate
-if [ ! -f "/etc/lets_encrypt/live/$CERTBOT_DOMAIN/fullchain.pem" ]; then
+# Check that all certificates for all domains exist, if not then create them
+missing_domains=()
+
+for domain_name in "${domain_array[@]}";
+do
+  if [ ! -f "/etc/lets_encrypt/live/$domain_name/fullchain.pem" ]; then
+    missing_domains+=(domain_name)
+  fi
+done
+
+# If array not empty
+if [ ! ${#missing_domains[@]} -eq 0 ]; then
   certbot certonly \
     -a dns-namecheap \
     --dns-namecheap-credentials=/namecheap.ini \
     --agree-tos --non-interactive -vv \
     --no-eff-email \
     --email "$CERTBOT_EMAIL" \
-    --domain "$CERTBOT_DOMAIN"
+    --domains "$(IFS=, ; echo "${missing_domains[*]}")"
 fi
 
 # Renewal loop
