6067ca91c931f9605c6618a3c2cce4f617efb960
Nathan Guerrero <naguerr2@uci.edu>
2026-01-17 04:53:05 +0000

    fix: Have certbot-entrypoint-helper output unbuffered, in nginx-vps1

--- nginx-vps1/certbot-entrypoint.sh
+++ nginx-vps1/certbot-entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 trap "exit" TERM
 
 # Run Python helper script, for more functionality
-python /certbot-entrypoint-helper.py
+python -u /certbot-entrypoint-helper.py
 
 # Renewal loop
 while true; do
