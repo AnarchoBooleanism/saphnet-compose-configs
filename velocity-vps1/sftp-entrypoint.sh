2ddc966440d10fbdf60489f654a4c0786c6940d0
Nathan Guerrero <naguerr2@uci.edu>
2026-01-17 11:54:29 +0000

    feat: Add Velocity server config for vps1

---
+++ velocity-vps1/sftp-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+trap "exit" TERM
+
+# Create /app/config/sftp.json (login details, directory information)
+cat > /app/config/sftp.json <<EOF
+{
+    "Global": {
+        "Chroot": {
+            "Directory": "%h",
+            "StartPath": "sftp"
+        },
+        "Directories": ["sftp"]
+    },
+    "Users": [
+        {
+            "Username": "${SFTP_USERNAME}",
+            "Password": "${SFTP_PASSWORD}"
+        }
+    ]
+}
+EOF
+
+tini -- dotnet ES.SFTP.dll
