2597cc4816c432a3c48c267bd44c3c8edc248360
Nathan Guerrero <anarchbool@anarchbool.dev>
2026-09-24 18:41:18 -0700

    feat: Add welcome terminal message to velocity-vps1 stack

---
+++ stacks/velocity-vps1/scripts/welcome.sh
@@ -0,0 +1,5 @@
+# Welcome message to inform users about how to enter RCON
+
+printf 'Welcome to the terminal for the Velocity container!\n\n'
+printf 'To enter Velocity via RCON, run the "rcon-cli" command.\n'
+printf 'No extra arguments or environment variables are needed.\n'
